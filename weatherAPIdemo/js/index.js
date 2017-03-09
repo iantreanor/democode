4dda8f89c760afc7f3e59c5bf09ee92c31bc472c
iantreanor <trei1_15@uni.worc.ac.uk>
2017-03-09 10:09:15 +0000

    dsd

--- weatherAPIdemo/js/index.js
+++ weatherAPIdemo/js/index.js
@@ -6,7 +6,7 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	<!-- Use an HTML GET request to obtain data from a Yahoo Pipe
 	<!-- The Yahoo pipe currently parses the BBC News RSS feed  -->
 	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", feedURL, false);
+	xmlhttp.open("GET", https://www.metaweather.com/api/location/search/?query=london, false);
 	xmlhttp.send();
 		
 		
